762fe09c5194814b080ef8633e4965c31bb7723e
GlenneJSG <glenne.grossman@gmail.com>
2018-01-13 14:08:44 -0500

    now its fixed?

---
+++ parachute/src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBaV6_73x0VHrkLvYwnvbTpNadhRPal14A",
+  authDomain: "parachute-191a7.firebaseapp.com",
+  databaseURL: "https://parachute-191a7.firebaseio.com",
+  projectId: "parachute-191a7",
+  storageBucket: "parachute-191a7.appspot.com",
+  messagingSenderId: "1039836428906"
+};
